5817c0e1db20cf0d29fea6b1b74b23931286d635
Daniel Santos <alexandersantos96@outlook.com>
2025-07-22 13:10:35 -0600

    Creacion de CRUD  de producto con modelViewSet

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    marca = models.CharField(max_length=50)
+    cantidad_minima = models.IntegerField()
+    cantidad_maxima = models.IntegerField()
+    precio = models.DecimalField(max_digits=5, decimal_places=2)
+
