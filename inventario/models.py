3ce74f0fd655736737af70c2dfb0881104a2be68
Jav1025 <am1545012010@unab.edu.sv>
2025-07-24 02:37:59 +0000

    agregue en el model

--- inventario/models.py
+++ inventario/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Creaando los modelos.
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
